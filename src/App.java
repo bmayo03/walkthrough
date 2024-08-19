77d4b8abea9d78d8851c7753e285de570ce79cdc
unknown <bmayo@luc.edu>
2024-08-18 21:09:50 -0500

    Updated the price variable to 33

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App{
 	public static void main(String[] args) {
 
-	   double price = 23.00;
+	   double price = 33.00;
 	   double discount = .10;
 	   price = (1 - discount) * price;
 	   System.out.println("With discount the price is " +  price); 
